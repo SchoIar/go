c449e32285c3745253472c5f39a05f155c8657a8
Anton <antonilic7@gmail.com>
2024-12-26 16:11:44 -0800

    Added support for operations in board

--- src/main/java/com/go/Level.java
+++ src/main/java/com/go/Level.java
@@ -8,7 +8,7 @@ import java.util.List;
  */
 public class Level {
     private Board board;
-    private List<int[]> solution; //lists ordered set of moves to solve the puzzle. 
+    private List<int[]> solution; //lists ordered set of moves to solve the puzzle, in format x,y
     private int current;
 
     public Level(Board board, List<int[]> solution) {
@@ -17,7 +17,24 @@ public class Level {
         this.current = 0; 
     }
 
-    public boolean isCorrectMove(int x, int y, boolean isWhite){
+    public Level(Board board, List<int[]> solution, List<int[]> initialWhiteStone, List<int[]> initialBlackStones) {
+        this.board = board;
+        this.solution = solution;
+        this.current = 0; 
+
+        board.setIntialStones(initialWhiteStone, initialBlackStones);
+
+    }
+
+    /**
+     * Checks if current move is the correct move. If so, it can be played, else, level resets.
+     * 
+     * @param x
+     * @param y
+     * @param isWhite
+     * @return
+     */
+    public boolean playMove(int x, int y, boolean isWhite){
         int[] expectedMove = solution.get(current);
         if(x == expectedMove[0] && y == expectedMove[1]){
             board.play(x, y, isWhite);
@@ -33,6 +50,14 @@ public class Level {
     public void reset() {
         board.restart();
         current = 0; 
+    }
+
+    public void print(){
+        board.print();
+    }
+
+    public boolean isSolved(){
+        return current >= solution.size();
     }
 
 }
