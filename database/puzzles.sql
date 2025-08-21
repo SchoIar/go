265610aae3dc727fc0023b562f6cfd9e3ca2ea1b
Anton <antonilic7@gmail.com>
2025-08-21 15:02:00 -0700

    added 2nd puzzle

--- database/puzzles.sql
+++ database/puzzles.sql
@@ -5,5 +5,15 @@ INSERT INTO puzzles (
     1,
     '[[0,1]]',
     '[[0,0]]',
-    '[[1,1]]'
+    '[[1,0]]'
+);
+
+INSERT INTO puzzles (
+    name, difficulty, initial_white, initial_black, solution
+) VALUES (
+    'Second Capture',
+    1,
+    '[[0,1]]',
+    '[[0,0], [0, 3], [3, 0]]',
+    '[[1,0]]'
 );
